fe77f5efcf3e0b0036b67af3ea077d0935386815
Jesse <jbuck2014@gmail.com>
2022-03-28 17:05:33 -0400

    [Jira-ID] - Base class structure and unit cases added

---
+++ src/main/java/PrimeNumberGenerator.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+interface PrimeNumberGenerator {
+    List<Integer> generate(int startingValue, int endingValue);
+    boolean isPrime(int value);
+
+}
---
+++ src/test/java/PrimeNumberGeneratorImplTest.java
@@ -0,0 +1,71 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PrimeNumberGeneratorImplTest {
+
+    PrimeNumberGenerator primeNumberGenerator = new PrimeNumberGeneratorImpl();
+
+    @Test
+    public void isPrimeNegativeTest() {
+        assertFalse(primeNumberGenerator.isPrime(-1));
+    }
+
+    @Test
+    public void isPrimeNegativeTest2() {
+        assertFalse(primeNumberGenerator.isPrime(-999999));
+    }
+
+    @Test
+    public void isPrimeZeroTest() {
+        assertFalse(primeNumberGenerator.isPrime(0));
+    }
+
+    @Test
+    public void isPrimeOneTest() {
+        assertFalse(primeNumberGenerator.isPrime(1));
+    }
+
+    @Test
+    public void isPrimeTwoTest() {
+        assertTrue(primeNumberGenerator.isPrime(2));
+    }
+
+    @Test
+    public void generateTestNone() {
+        List<Integer> primeList = primeNumberGenerator.generate(-999,1);
+        assertEquals(0, primeList.size());
+    }
+
+
+    @Test
+    public void generateTestOneToTen() {
+        List<Integer> primeList = primeNumberGenerator.generate(1,10);
+        assertEquals(4, primeList.size());
+        assertTrue(primeList.contains(2));
+        assertTrue(primeList.contains(3));
+        assertTrue(primeList.contains(5));
+        assertTrue(primeList.contains(7));
+    }
+
+    @Test
+    public void generateTestTenToOne() {
+        List<Integer> primeList = primeNumberGenerator.generate(10,1);
+        assertEquals(4, primeList.size());
+        assertTrue(primeList.contains(2));
+        assertTrue(primeList.contains(3));
+        assertTrue(primeList.contains(5));
+        assertTrue(primeList.contains(7));
+    }
+
+    @Test
+    public void generateTestLarge() {
+        List<Integer> primeList = primeNumberGenerator.generate(7900,7920);
+        assertEquals(3, primeList.size());
+        assertTrue(primeList.contains(7901));
+        assertTrue(primeList.contains(7907));
+        assertTrue(primeList.contains(7919));
+    }
+}
